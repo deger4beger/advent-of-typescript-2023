95f81641576d5bbf7f354ef69dfe806fe8c41222
Danila Kochinov <deger.begerrr@gmail.com>
2023-12-20 14:32:26 +0600

    Create day-20.ts

---
+++ day-20.ts
@@ -0,0 +1,162 @@
+// Solution
+type Letters = {
+  A: [
+    '█▀█ ',
+    '█▀█ ',
+    '▀ ▀ ',
+  ],
+  B: [
+    '█▀▄ ',
+    '█▀▄ ',
+    '▀▀  '
+  ],
+  C: [
+    '█▀▀ ',
+    '█ ░░',
+    '▀▀▀ '
+  ],
+  E: [
+    '█▀▀ ',
+    '█▀▀ ',
+    '▀▀▀ '
+  ],
+  H: [
+    '█ █ ',
+    '█▀█ ',
+    '▀ ▀ '
+  ],
+  I: [
+    '█ ',
+    '█ ',
+    '▀ '
+  ],
+  M: [
+    '█▄░▄█ ',
+    '█ ▀ █ ',
+    '▀ ░░▀ '
+  ],
+  N: [
+    '█▄░█ ',
+    '█ ▀█ ',
+    '▀ ░▀ '
+  ],
+  P: [
+    '█▀█ ',
+    '█▀▀ ',
+    '▀ ░░'
+  ],
+  R: [
+    '█▀█ ',
+    '██▀ ',
+    '▀ ▀ '
+  ],
+  S: [
+    '█▀▀ ',
+    '▀▀█ ',
+    '▀▀▀ '
+  ],
+  T: [
+    '▀█▀ ',
+    '░█ ░',
+    '░▀ ░'
+  ],
+  Y: [
+    '█ █ ',
+    '▀█▀ ',
+    '░▀ ░'
+  ],
+  W: [
+    '█ ░░█ ',
+    '█▄▀▄█ ',
+    '▀ ░ ▀ '
+  ],
+  ' ': [
+    '░',
+    '░',
+    '░'
+  ],
+  ':': [
+    '#',
+    '░',
+    '#'
+  ],
+  '*': [
+    '░',
+    '#',
+    '░'
+  ],
+};
+
+type GetCurrentSymbol<Key extends keyof Letters, Index extends number> = Letters[Key][Index]
+type ReplaceLine<T1 extends string, IterationCycle extends number> = 
+  T1 extends `${infer First extends keyof Letters}${infer Rest}` 
+    ? `${GetCurrentSymbol<First, IterationCycle>}${ReplaceLine<Rest, IterationCycle>}` 
+    : ''
+  
+type FillLines<T extends string, Result extends string[] = []> = 
+  Result['length'] extends 3
+    ? Result
+    : FillLines<T, [...Result, ReplaceLine<T, Result['length']>]>
+
+type ToAsciiArt<T extends string, Result extends string[] = []> = 
+  T extends `${infer Head}\n${infer Rest}`
+    ? ToAsciiArt<Rest, [...Result, ...FillLines<Uppercase<Head>>]>
+    : [...Result, ...FillLines<Uppercase<T>>]
+
+
+// Tests
+import { Equal, Expect } from "type-testing";
+
+type test_0_actual = ToAsciiArt<"   * : * Merry * : *   \n  Christmas  ">;
+//   ^?
+type test_0_expected = [
+  "░░░░░#░░░█▄░▄█ █▀▀ █▀█ █▀█ █ █ ░░░#░░░░░",
+  "░░░#░░░#░█ ▀ █ █▀▀ ██▀ ██▀ ▀█▀ ░#░░░#░░░",
+  "░░░░░#░░░▀ ░░▀ ▀▀▀ ▀ ▀ ▀ ▀ ░▀ ░░░░#░░░░░",
+  "░░█▀▀ █ █ █▀█ █ █▀▀ ▀█▀ █▄░▄█ █▀█ █▀▀ ░░",
+  "░░█ ░░█▀█ ██▀ █ ▀▀█ ░█ ░█ ▀ █ █▀█ ▀▀█ ░░",
+  "░░▀▀▀ ▀ ▀ ▀ ▀ ▀ ▀▀▀ ░▀ ░▀ ░░▀ ▀ ▀ ▀▀▀ ░░",
+];
+type test_0 = Expect<Equal<test_0_actual, test_0_expected>>;
+
+type test_1_actual = ToAsciiArt<"  Happy new  \n  * : * : * Year * : * : *  ">;
+//   ^?
+type test_1_expected = [
+        "░░█ █ █▀█ █▀█ █▀█ █ █ ░█▄░█ █▀▀ █ ░░█ ░░",
+        "░░█▀█ █▀█ █▀▀ █▀▀ ▀█▀ ░█ ▀█ █▀▀ █▄▀▄█ ░░",
+        "░░▀ ▀ ▀ ▀ ▀ ░░▀ ░░░▀ ░░▀ ░▀ ▀▀▀ ▀ ░ ▀ ░░",
+        "░░░░#░░░#░░░█ █ █▀▀ █▀█ █▀█ ░░░#░░░#░░░░",
+        "░░#░░░#░░░#░▀█▀ █▀▀ █▀█ ██▀ ░#░░░#░░░#░░",
+        "░░░░#░░░#░░░░▀ ░▀▀▀ ▀ ▀ ▀ ▀ ░░░#░░░#░░░░",
+];
+type test_1 = Expect<Equal<test_1_actual, test_1_expected>>;
+
+type test_2_actual = ToAsciiArt<"  * : * : * : * : * : * \n  Trash  \n  * : * : * : * : * : * ">;
+//   ^?
+type test_2_expected = [
+  "░░░░#░░░#░░░#░░░#░░░#░░░",
+  "░░#░░░#░░░#░░░#░░░#░░░#░",
+  "░░░░#░░░#░░░#░░░#░░░#░░░",
+  "░░▀█▀ █▀█ █▀█ █▀▀ █ █ ░░",
+  "░░░█ ░██▀ █▀█ ▀▀█ █▀█ ░░",
+  "░░░▀ ░▀ ▀ ▀ ▀ ▀▀▀ ▀ ▀ ░░",
+  "░░░░#░░░#░░░#░░░#░░░#░░░",
+  "░░#░░░#░░░#░░░#░░░#░░░#░",
+  "░░░░#░░░#░░░#░░░#░░░#░░░",
+];
+type test_2 = Expect<Equal<test_2_actual, test_2_expected>>;
+
+type test_3_actual = ToAsciiArt<"  : * : * : * : * : * : * : \n  Ecyrbe  \n  : * : * : * : * : * : * : ">;
+//   ^?
+type test_3_expected = [
+  "░░#░░░#░░░#░░░#░░░#░░░#░░░#░",
+  "░░░░#░░░#░░░#░░░#░░░#░░░#░░░",
+  "░░#░░░#░░░#░░░#░░░#░░░#░░░#░",
+  "░░█▀▀ █▀▀ █ █ █▀█ █▀▄ █▀▀ ░░",
+  "░░█▀▀ █ ░░▀█▀ ██▀ █▀▄ █▀▀ ░░",
+  "░░▀▀▀ ▀▀▀ ░▀ ░▀ ▀ ▀▀  ▀▀▀ ░░",
+  "░░#░░░#░░░#░░░#░░░#░░░#░░░#░",
+  "░░░░#░░░#░░░#░░░#░░░#░░░#░░░",
+  "░░#░░░#░░░#░░░#░░░#░░░#░░░#░",
+];
+type test_3 = Expect<Equal<test_3_actual, test_3_expected>>;
